15ad3f1564a685630cbea07103cf0f5d079e753d
Roman Dodin <dodin.roman@gmail.com>
2023-12-14 12:31:52 +0200

    bump netcommon to support custom ca cert

--- run.sh
+++ run.sh
@@ -123,6 +123,12 @@ function test-tls-skip {
   ansible-playbook playbooks/tls-skipped-check.yml "$@"
 }
 
+function test-tls-custom-ca {
+  _cdTests
+  revert-to-checkpoint
+  ansible-playbook playbooks/tls-with-custom-ca.yml "$@"
+}
+
 function test-get-container {
   _cdTests
   revert-to-checkpoint
@@ -276,6 +282,7 @@ function _run-tests {
   test-cli-wrong-cmd "$@"
   test-tls-fail "$@"
   test-tls-skip "$@"
+  test-tls-custom-ca "$@"
   test-set-check-mode "$@"
   test-set-leaves "$@"
   test-set-leaves-twice "$@"
